4e08705e3c596a04876683e24e91e9ba9e707419
joyalshaji135 <joyalshaji135@gmail.com>
2023-10-24 23:39:44 +0530

    Static Block is Completed

---
+++ Basic-Oops-in-Java/Static_Block.java
@@ -0,0 +1,18 @@
+public class Static_Block {
+    int number;
+    String name;
+
+    // Static Block 
+    static{
+        // number = 100;
+        // name = "Joyal Shaji";
+
+        int number1 = 100;
+        String name1 = "Joyal";
+        System.out.println("Number is :"+number1);
+        System.out.println("Name is :"+name1);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
