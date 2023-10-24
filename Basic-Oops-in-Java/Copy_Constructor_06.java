b88f0bdabf825c86b448ba541fd9b45ee7488bdc
joyalshaji135 <joyalshaji135@gmail.com>
2023-10-24 19:31:11 +0530

    Copy Constructor is Completed

--- Basic-Oops-in-Java/Copy_Constructor_06.java
+++ Basic-Oops-in-Java/Copy_Constructor_06.java
@@ -20,5 +20,40 @@ public class Copy_Constructor_06 {
     public static void main(String[] args) {
         Copy c = new Copy();
         Copy c1 = new Copy(c);
+        Copy2 c2 = new Copy2(100, "Sreemayi CM");
+        Copy2 c3 = new Copy2(c2);
+        c3.display();
+        c2.display1();
+    }
+}
+
+class Copy2
+{
+    int number1;
+    String name1;
+    int number2;
+    String name2;
+    Copy2(int num1,String name)
+    {
+        number1 = num1;
+        name1 = name;
+    }
+
+    Copy2(Copy2 obj)
+    {
+        number2 = obj.number1;
+        name2 = obj.name1;
+    }
+
+    void display()
+    {
+        System.out.println("Number 2 is :"+number2);
+        System.out.println("Name 2 is :"+name2);
+    }
+
+    void display1()
+    {
+        System.out.println("Number 1 is :"+number1);
+        System.out.println("Name 1 is :"+name1);
     }
 }
