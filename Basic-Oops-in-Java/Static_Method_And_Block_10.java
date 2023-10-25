cd48081cb517dbb8a24d71a4f796146d7df99722
joyalshaji135 <joyalshaji135@gmail.com>
2023-10-25 23:20:00 +0530

    Static And Instance

---
+++ Basic-Oops-in-Java/Static_Method_And_Block_10.java
@@ -0,0 +1,14 @@
+
+public class Static_Method_And_Block_10 {
+    static
+    {
+        System.out.println("Static Block");
+    }    
+    static void show()
+    {
+        System.out.println("Static Method");
+    }
+    public static void main(String[] args) {
+        Static_Method_And_Block_10.show();
+    }
+}
