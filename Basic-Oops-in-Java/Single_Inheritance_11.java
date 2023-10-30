92fdcf49c4df73c9439921b92f631b40e7dd1264
joyalshaji135 <joyalshaji135@gmail.com>
2023-10-30 21:48:35 +0530

    Single Inhertiance

--- Basic-Oops-in-Java/Single_Inheritance_11.java
+++ Basic-Oops-in-Java/Single_Inheritance_11.java
@@ -1,24 +1,23 @@
-class College
-{
+import java.util.Scanner;
+
+class College {
     int roll;
     int mark;
     String name;
-    void score()
-    {
+
+    void score() {
         System.out.println("Student Roll no , Mark and Name:");
     }
 }
 
-class Student extends College
-{
-    void show()
-    {
+class Student extends College {
+    void show() {
         roll = 10;
         mark = 200;
         name = "Joyal Shaji";
-        System.out.println("Roll no is :"+roll);
-        System.out.println("Mark is :"+mark);
-        System.out.println("Student Name is :"+name);
+        System.out.println("Roll no is :" + roll);
+        System.out.println("Mark is :" + mark);
+        System.out.println("Student Name is :" + name);
     }
 }
 
@@ -27,5 +26,32 @@ public class Single_Inheritance_11 {
         Student s = new Student();
         s.score();
         s.show();
-    }    
+        System.out.println("_________________________________________");
+        Employee e = new Employee();
+        e.Input_Data();
+        e.Display_Inputs();
+    }
+}
+
+class Company {
+    int Reg_no;
+    String Employee_Name;
+    float Salary;
+    void Input_Data() {
+        System.out.println("Enter the Company Employee Reg no: ");
+        Scanner sc = new Scanner(System.in);
+        Reg_no = sc.nextInt();
+        System.out.println("Enter the Employee Name :");
+        Employee_Name = sc.next();
+        System.out.println("Enter the Salary :");
+        Salary = sc.nextFloat();
+    }
+}
+
+class Employee extends Company {
+    void Display_Inputs() {
+        System.out.println("Employee Reg No :"+Reg_no);
+        System.out.println("Employee Name :"+Employee_Name);
+        System.out.println("Employee Salary :"+Salary);
+    }
 }
