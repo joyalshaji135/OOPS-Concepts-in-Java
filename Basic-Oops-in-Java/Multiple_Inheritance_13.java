30382fddbf73db11a444660057ce7a8fdcf8bcf6
joyalshaji135 <joyalshaji135@gmail.com>
2023-11-07 15:41:03 +0530

    Multiple Inheritance is Completed

---
+++ Basic-Oops-in-Java/Multiple_Inheritance_13.java
@@ -0,0 +1,25 @@
+class Super_Class_01
+{
+    void Display_01()
+    {
+        System.out.println("Super Class 01");
+    }
+}
+
+class Super_Class_02
+{
+    void Display_02()
+    {
+        System.out.println("Super Class 02");
+    }
+}
+
+
+public class Multiple_Inheritance_13 extends Super_Class_01,Super_Class_02 {
+    public static void main(String[] args) {
+        // is Confusion 
+        // Eg
+        MultiLevel_Inheritance_12 m = new MultiLevel_Inheritance_12();
+        m.Display_01();
+    }
+}
